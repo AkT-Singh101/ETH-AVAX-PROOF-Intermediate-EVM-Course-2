574ef49b1ce5a6261c373486d432d32f29110cdf
AkT-Singh101 <126188221+AkT-Singh101@users.noreply.github.com>
2024-07-10 22:46:32 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("GOJO", "JJK") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
+    }
+}
